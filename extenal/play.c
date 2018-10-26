f3c8892deb6cc079c264a6dc9b6097c769b51200
PinkD <443657547@qq.com>
2018-10-26 23:23:09 +0800

    replace alsa with openal to cross platform
    	修改：     ../CMakeLists.txt
    	修改：     ../extenal/play.c
    
     位于分支 master
     要提交的变更：
    	修改：     ../CMakeLists.txt
    	修改：     ../extenal/play.c
    
     尚未暂存以备提交的变更：
    	修改：     ../CMakeLists.txt
    	修改：     ../extenal/play.c

--- extenal/play.c
+++ extenal/play.c
@@ -1,62 +1,72 @@
 #include "play.h"
 #include "../log.h"
 
-#define PCM_DEVICE "default"
+#include <AL/al.h>
+#include <AL/alc.h>
+
+
+static inline ALenum to_al_format(short channels, short samples) {
+    int stereo = (channels > 1);
+    switch (samples) {
+        case 16:
+            if (stereo) {
+                return AL_FORMAT_STEREO16;
+            } else {
+                return AL_FORMAT_MONO16;
+            }
+        case 8:
+            if (stereo) {
+
+                return AL_FORMAT_STEREO8;
+            } else {
+                return AL_FORMAT_MONO8;
+            }
+        default:
+            return -1;
+    }
+}
+
 
 void play(Wave *wave) {
-    uint rate, channel, tmp;
-    int ret;
-    snd_pcm_t *pcm_handle;
-    snd_pcm_hw_params_t *params;
-    snd_pcm_uframes_t frames;
-    size_t buff_size, loops;
+    ALCdevice *device = alcOpenDevice(NULL);
 
-    rate = wave->header->fmt.sample_rate;
-    channel = wave->header->fmt.channel;
-//    seconds = 200;
+    if (!device) {
+        log_e("Fail to open device");
+        return;
+    }
 
-    /* Open the PCM device in playback mode */
-    if ((ret = snd_pcm_open(&pcm_handle, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK, 0)) < 0)
-        printf("ERROR: Can't open \"%s\" PCM device. %s\n",
-               PCM_DEVICE, snd_strerror(ret));
+    ALCcontext *context = alcCreateContext(device, NULL);
+    if (!alcMakeContextCurrent(context)) {
+        log_e("Fail to make context");
+        return;
+    }
 
-    /* Allocate parameters object and fill it with default values*/
-    snd_pcm_hw_params_alloca(&params);
+    ALfloat listenerOri[] = {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f};
 
-    snd_pcm_hw_params_any(pcm_handle, params);
+    alListener3f(AL_POSITION, 0, 0, 1.0f);
+    alListener3f(AL_VELOCITY, 0, 0, 0);
+    alListenerfv(AL_ORIENTATION, listenerOri);
 
-    /* Set parameters */
-    if ((ret = snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0)
-        printf("ERROR: Can't set interleaved mode. %s\n", snd_strerror(ret));
+    ALuint source;
 
-    if ((ret = snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S16_LE)) < 0)
-        printf("ERROR: Can't set format. %s\n", snd_strerror(ret));
+    alGenSources(1, &source);
 
-    if ((ret = snd_pcm_hw_params_set_channels(pcm_handle, params, channel)) < 0)
-        printf("ERROR: Can't set channels number. %s\n", snd_strerror(ret));
-
-    if ((ret = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &rate, 0)) < 0)
-        printf("ERROR: Can't set rate. %s\n", snd_strerror(ret));
-
-    /* Write parameters */
-    if ((ret = snd_pcm_hw_params(pcm_handle, params)) < 0)
-        printf("ERROR: Can't set harware parameters. %s\n", snd_strerror(ret));
-
-    /* Allocate buffer to hold single period */
-    snd_pcm_hw_params_get_period_size(params, &frames, 0);
-
-    buff_size = frames * channel * 2 /* 2 -> sample size */;
-    size_t count = 0;
+    alSourcef(source, AL_PITCH, 1);
+    alSourcef(source, AL_GAIN, 1);
+    alSource3f(source, AL_POSITION, 0, 0, 0);
+    alSource3f(source, AL_VELOCITY, 0, 0, 0);
+    alSourcei(source, AL_LOOPING, AL_FALSE);
 
 
-    snd_pcm_hw_params_get_period_time(params, &tmp, NULL);
+    ALuint buffer;
+    alGenBuffers(1, &buffer);
+
 
     WaveData *data = wave->data;
     while (!(data->data_chunk_header[0] == 'd' &&
              data->data_chunk_header[1] == 'a' &&
              data->data_chunk_header[2] == 't' &&
-             data->data_chunk_header[3] == 'a')
-            ) {
+             data->data_chunk_header[3] == 'a')) {
         data = data->next;
         if (!data) {
             log_w("no available data to play");
@@ -64,17 +74,30 @@ void play(Wave *wave) {
         }
     }
 
-    for (loops = data->data_size / buff_size; loops > 0; loops--) {
-        if ((ret = (int) snd_pcm_writei(pcm_handle, data->data + count, frames)) == -EPIPE) {
-            printf("XRUN.\n");
-            snd_pcm_prepare(pcm_handle);
-        } else if (ret < 0) {
-            printf("ERROR. Can't write to PCM device. %s\n", snd_strerror(ret));
-        }
-        count += buff_size;
+    byte *bufferData = malloc(data->data_size);
+    memcpy(bufferData, data->data, data->data_size);
+    alBufferData(buffer,
+                 to_al_format(wave->header->fmt.channel, wave->header->fmt.bits_per_sample),
+                 bufferData,
+                 data->data_size,
+                 wave->header->fmt.sample_rate);
+
+
+    alSourcei(source, AL_BUFFER, buffer);
+    alSourcePlay(source);
+
+    ALint source_state;
+    alGetSourcei(source, AL_SOURCE_STATE, &source_state);
+    while (source_state == AL_PLAYING) {
+        alGetSourcei(source, AL_SOURCE_STATE, &source_state);
     }
 
-    snd_pcm_drain(pcm_handle);
-    snd_pcm_close(pcm_handle);
+    alDeleteSources(1, &source);
+    alDeleteBuffers(1, &buffer);
 
+    alcMakeContextCurrent(NULL);
+
+    alcDestroyContext(context);
+    alcCloseDevice(device);
+    free(bufferData);
 }
